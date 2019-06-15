da69a0e670b598c9321ba4740d472a642871eae6
Levi-Huynh <Levi_huynh@yahoo.com>
2019-06-15 10:07:35 -0700

    mvp with login register and jokes routes

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -2,11 +2,14 @@ const jwt = require('jsonwebtoken');
 
 const jwtKey =
   process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+  'Shh this is the secret here!';
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  jwtKey:
+  process.env.JWT_SECRET ||
+  'Shh this is the secret here!',
 };
 
 // implementation details
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
-
-const { authenticate } = require('../auth/authenticate');
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Users = require('../users/users-model.js');
+const { authenticate } = require('../auth/authenticate.js');
+const secrets = require('../auth/authenticate.js'); 
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,11 +13,66 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
-}
+ let user = req.body;
+  const hash =  bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+ Users.add(user)
+    .then(user => {
+      console.log(user);
+      const token = generateToken(user);
+      res.status(201).json({user,token});
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+ }
+
+
+
+// async function register (req, res) {
+//   try{ let user = req.body;
+//    const hash = await bcrypt.hashSync(user.password, 10); // 2 ^ n
+//    user.password = hash;
+ 
+//   const reg = await Users.add(user);
+//     if(reg) {
+//        res.status(201).json(reg);
+//      }else{
+    
+//        res.status(500).json(error); }
+//      }catch (error) {
+//       res
+//       .status(500)
+//       .json({ message: 'We ran into error registering user' });
+//   }
+// };
+
+
+
+
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // req.session.username = user.username;
+     //the cookie is sent by the express-session library 
+     const token= generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials, You shall not pass!' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
@@ -29,4 +88,27 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+
+
+function generateToken(user) {
+  //payload is data
+  //nomrally good to store secret in different file location
+  const payload = {
+    //docs will show you options for your payload, secret ,options 
+    //subject - who is this token identifying
+    //docs: npm jsonwebtoken
+    subject: user.id, //what the token is describing
+    username: user.username,
+    roles: user.department, //normally would be found in db user.roles
+    };
+
+  
+
+    const options = {
+      expiresIn: '1h',
+    };
+
+  return jwt.sign(payload, secrets.jwtKey, options)
 }
